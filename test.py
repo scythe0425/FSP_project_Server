92845c14b7620294f808a29b46025b51d3971429
scyth0425 <scythe0425@naver.com>
2025-05-08 11:43:50 +0900

    update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from pynput import keyboard
+from webbrowser import open_new_tab
 
 pressed_keys = set()
 
@@ -7,9 +8,14 @@ def on_press(key):
     print(f"Pressed: {key}")
     if (key == keyboard.KeyCode.from_char('a')):
         print("✅ 단축키 a 감지됨!")
+        open_new_tab("https://www.naver.com")
+
+        
     if(key==keyboard.Key.esc):
         print("❌ Esc 키 눌림. 프로그램 종료.")
         return False
+
+
 
 def on_release(key):
     if key in pressed_keys:
@@ -18,3 +24,4 @@ def on_release(key):
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     print("🔍 키보드 감지 시작. a 눌러봐!")
     listener.join()
+
